bf8342feab73cd1c11ad2d0fef1ddbe71c38fc78
Harlan T Wood <harlan@thegoldensun.com>
2012-12-17 16:26:56 -0500

    Only define Gollum::Page.cname if not defined already

--- lib/superstring/ext/gollum.rb
+++ lib/superstring/ext/gollum.rb
@@ -1,9 +1,9 @@
 module Gollum
   class Page
-    def self.cname(name, char_white_sub = '-', char_other_sub = '-')
-      name.respond_to?(:gsub) ?
-        name.gsub(%r{\s}, char_white_sub).gsub(%r{[/<>+]}, char_other_sub) :
-        ''
+    unless Page.respond_to?(:cname)
+      def self.cname(name, char_white_sub = '-', char_other_sub = '-')
+        name.respond_to?(:gsub) ? name.gsub(%r{\s}, char_white_sub).gsub(%r{[/<>+]}, char_other_sub) : ''
+      end
     end
   end
 end
